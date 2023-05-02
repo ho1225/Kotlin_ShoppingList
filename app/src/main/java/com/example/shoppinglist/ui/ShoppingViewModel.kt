d99607c247915390eb16e63491b77ce4f316224d
MSI\hosiu <hosiuchuen05@gmail.com>
2023-05-03 01:35:32 +0800

    REPOSITORY, VIEWMODEL, VIEWMODELFACTORY

--- app/src/main/java/com/example/shoppinglist/ui/ShoppingViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shoppinglist.ui
-
-import androidx.lifecycle.ViewModel
-import com.example.shoppinglist.data.repositories.ShoppingRepository
-
-class ShoppingViewModel(
-    private val repository: ShoppingRepository
-): ViewModel() {
-}
