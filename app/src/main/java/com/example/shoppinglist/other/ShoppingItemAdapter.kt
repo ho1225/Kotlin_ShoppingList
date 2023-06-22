532f6f31bbf2af70795284680a2e0ef6ad2e6e5a
MSI\hosiu <hosiuchuen05@gmail.com>
2023-06-22 09:02:06 +0800

    finish data binding for recycleview

--- app/src/main/java/com/example/shoppinglist/other/ShoppingItemAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/other/ShoppingItemAdapter.kt
@@ -3,6 +3,8 @@ package com.example.shoppinglist.other
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.example.shoppinglist.data.db.entities.ShoppingItem
@@ -13,12 +15,41 @@ class ShoppingItemAdapter (
     private val viewModel: ShoppingViewModel
 ): RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>(){
 
+
+    inner class ShoppingViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    {
+        var tvName: TextView = itemView.findViewById(R.id.tvName)
+        var tvAmount: TextView = itemView.findViewById(R.id.tvAmount)
+        var ivDelete: ImageView = itemView.findViewById(R.id.ivDelete)
+        var ivPlus: ImageView = itemView.findViewById(R.id.ivPlus)
+        var ivMinus: ImageView = itemView.findViewById(R.id.ivMinus)
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent, false)
         return ShoppingViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+        val curShoppingItem = items[position]
+
+        holder.tvName.text = curShoppingItem.name
+        holder.tvAmount.text = "${curShoppingItem.amount}"
+
+        holder.ivDelete.setOnClickListener {
+            viewModel.delete(curShoppingItem)
+        }
+
+        holder.ivPlus.setOnClickListener {
+            curShoppingItem.amount++
+            viewModel.upsert(curShoppingItem)
+        }
+
+        holder.ivMinus.setOnClickListener {
+            if(curShoppingItem.amount > 0) {
+                curShoppingItem.amount--
+                viewModel.upsert(curShoppingItem)
+            }
+        }
 
     }
 
@@ -26,5 +57,4 @@ class ShoppingItemAdapter (
         return items.size
     }
 
-    inner class ShoppingViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 }
